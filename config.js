d09569922452cbfa0db3e1f9c6c50675df3d25ff
Hammy Havoc <hammy@splitanatom.com>
2018-04-24 03:27:25 +0100

    Initial commit.

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'football-free-tips',
+		wp_ws_url : 'https://www.footballfreetips.com/wp-appkit-api/football-free-tips',
+		wp_url : 'https://www.footballfreetips.com',
+		theme : 'wpak-theme-bootstrap-feature-user-login',
+		version : '1.0',
+		app_type : 'phonegap-build',
+		app_title : 'Football Free Tips',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : '![FMbmjrB}ip,U:J>d=[R+>x/bC{$=i*KRtITDGYUTv/qzH9BZ#nT:(Z6RU]JaTk',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
